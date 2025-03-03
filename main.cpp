205fdb2f8a284f85796234fb4923e4617c84010f
LeonF3 <officialleonfranklin@gmail.com>
2025-03-03 10:57:54 -0500

    Calculator entry

--- main.cpp
+++ main.cpp
@@ -2,7 +2,39 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> num2;
+    double result;
+
+    switch(operation) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+        default:
+            // operator is doesn't match any case constant (+, -, *, /)
+            cout << "Error! The operator is not correct";
+            break;
+    }
+    cout << "Result: " << result << endl;
+    
+    
     return 0;
 }
  
